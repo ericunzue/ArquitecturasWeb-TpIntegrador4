42786e3bdc14cc5be2608e4374cead6b1e18cfbb
ericunzue <ericunzue@gmail.com>
2021-11-02 01:06:53 -0300

    agrega set de fecha en Compra

--- src/main/java/arq/integrador/despensa/entities/Compra.java
+++ src/main/java/arq/integrador/despensa/entities/Compra.java
@@ -1,11 +1,8 @@
 package arq.integrador.despensa.entities;
 
 
-
-
-
-
-import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,53 +11,66 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+
 
 @Entity
 public class Compra {
 
-		@Id
-		@GeneratedValue(strategy=GenerationType.AUTO)
-		private int idCompra;
-		@ManyToOne
-		@JoinColumn(name="idCliente")
-		private Cliente cliente;
-		//		private List<Producto> compras;
-		@Column
-		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss",timezone="GMT+8")
-		private Date fecha;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int idCompra;
+	@ManyToOne
+	@JoinColumn(name="idCliente")
+	private Cliente cliente;
+	//		private List<Producto> compras;
 
-		public Compra() {
+//	
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	@CreationTimestamp
+	private LocalDateTime  fecha;
 
-		}
+	public Compra() {
 
-		public Compra(Cliente cliente) {
-				super();
-				this.cliente = cliente;
-				long miliseconds = System.currentTimeMillis();				
-				this.fecha = new Date(miliseconds);			
-			
-			
-		}
+	}
 
-		public Cliente getCliente() {
-				return cliente;
-		}
+	public Compra(Cliente cliente) {
+		super();
+		this.cliente = cliente;	
 
-		public void setCliente(Cliente cliente) {
-				this.cliente = cliente;
-		}
 
-		public int getIdCompra() {
-				return idCompra;
-		}
+	}
 
-		public Date getFecha() {
-			return fecha;
-		}
+	public Cliente getCliente() {
+		return cliente;
+	}
 
-	
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public int getIdCompra() {
+		return idCompra;
+	}
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	//	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	//	protected Date onCreate() { 
+	//		return new Date(); 
+	//	}
+
+
 
 
 
