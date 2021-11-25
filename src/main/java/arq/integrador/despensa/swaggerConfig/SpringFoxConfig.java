7753dca531b5d707928ad6a46fc2ba073f6bf4f4
ericunzue <ericunzue@gmail.com>
2021-11-24 22:43:46 -0300

    agrega swagger documentation

--- src/main/java/arq/integrador/despensa/swaggerConfig/SpringFoxConfig.java
+++ src/main/java/arq/integrador/despensa/swaggerConfig/SpringFoxConfig.java
@@ -9,8 +9,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-
-public class SwaggerConfig {
+public class SpringFoxConfig {
 	
 	@Bean
 	public Docket apiDocket() {
